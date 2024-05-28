1cd93e67097509c5637507f02657716a8a7ebabb
cyberspacex <126796642+oluwatay123@users.noreply.github.com>
2024-05-28 17:14:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-// app.use(express.static("public"));
-app.get('/', (req, res) => res.send(`'THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED
-WITH AWS. THIS PAGE INDICATES SUCCESS. \n
-YOURNAME -----------------  \n
-MATRIC NUMBER ------------'`));
+app.use(express.static("public"));
+// app.get('/', (req, res) => res.send(`'THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED
+// WITH AWS. THIS PAGE INDICATES SUCCESS. \n
+// YOURNAME -----------------  \n
+// MATRIC NUMBER ------------'`));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
