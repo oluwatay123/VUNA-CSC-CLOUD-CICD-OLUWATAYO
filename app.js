4bf495d74e3b226ea3f3cd056b6763f4fb1bbe3b
cyberspacex <126796642+oluwatay123@users.noreply.github.com>
2024-05-28 17:09:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 8080;
 // app.use(express.static("public"));
 app.get('/', (req, res) => res.send(`'THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED
 WITH AWS. THIS PAGE INDICATES SUCCESS. \n
