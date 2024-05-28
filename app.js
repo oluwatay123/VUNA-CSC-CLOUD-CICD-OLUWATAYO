8d8acadc1dd3bb609dec5c18ea5eab3f622c4492
cyberspaceX <oluwatayodavid273@gmail,com>
2024-05-28 12:26:12 +0100

    update

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 8000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send(`'THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED
+WITH AWS. THIS PAGE INDICATES SUCCESS. \n
+YOURNAME -----------------  \n
+MATRIC NUMBER ------------'`));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
